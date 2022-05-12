d07a85d1574c51b45cca151a03544d0e80d35636
greinerk <greinerk@gmail.com>
2022-05-12 11:03:23 -0400

    add batch files for mssql-scripter

---
+++ sqltools/SqlVersionControl/mssql-scripter/pca-sql-schema.bat
@@ -0,0 +1,34 @@
+
+rem Parameters
+rem %1 server name
+rem %2 db name
+rem %3 SQL scripts root path
+
+if "%~*"=="" GOTO exit
+
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2 --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types StoredProcedure Table UserDefinedAggregate UserDefinedDataType UserDefinedFunction UserDefinedTableTypes UserDefinedType View
+
+cd %3\%1\%2
+
+mkdir UserDefinedFunctions
+move *.UserDefinedFunction.sql UserDefinedFunctions
+
+mkdir StoredProcedures
+move *.StoredProcedure.sql StoredProcedures
+
+mkdir Tables
+move *.Table.sql Tables
+
+mkdir UserDefinedDataTypes
+move *.UserDefinedDataType.sql UserDefinedDataTypes
+
+mkdir Views
+move *.View.sql Views
+
+mkdir UserDefinedTableTypes
+move *.UserDefinedTableType.sql UserDefinedTableTypes
+
+mkdir UserDefinedAggregates
+move *.UserDefinedAggregate.sql UserDefinedAggregates
+
+:exit
