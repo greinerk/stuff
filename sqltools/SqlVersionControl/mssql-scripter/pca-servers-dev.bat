e6ebec2d917a61c3ffddcc1df872edfd96b01fdd
greinerk <greinerk@gmail.com>
2022-05-12 12:19:35 -0400

    support calling mssql-scripter from another batch file for multiple servers

--- sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev.bat
+++ sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev.bat
@@ -1,7 +1,7 @@
 
 pushd %~dp0
 
-call mssql-scripter awsql2019dev USAGG_DEV c:\pca\sql_schemas\mssql-scripter\
-call mssql-scripter awsql2019dev USALCO_FRQ_DEV c:\pca\sql_schemas\mssql-scripter\
+call pca-sql-schema.bat awsql2019dev USAGG_DEV c:\pca\sql_schemas\mssql-scripter\
+call pca-sql-schema.bat awsql2019dev USALCO_FRQ_DEV c:\pca\sql_schemas\mssql-scripter\
 
 popd
--- sqltools/SqlVersionControl/mssql-scripter/pca-sql-schema.bat
+++ sqltools/SqlVersionControl/mssql-scripter/pca-sql-schema.bat
@@ -1,14 +1,13 @@
+@echo off
 
 rem Parameters
 rem %1 server name
 rem %2 db name
 rem %3 SQL scripts root path
 
-if "%~*"=="" GOTO exit
-
 call mssql-scripter -S %1 -d %2 -f %3\%1\%2 --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types StoredProcedure Table UserDefinedAggregate UserDefinedDataType UserDefinedFunction UserDefinedTableTypes UserDefinedType View
 
-cd %3\%1\%2
+pushd %3\%1\%2
 
 mkdir UserDefinedFunctions
 move *.UserDefinedFunction.sql UserDefinedFunctions
@@ -31,4 +30,4 @@ move *.UserDefinedTableType.sql UserDefinedTableTypes
 mkdir UserDefinedAggregates
 move *.UserDefinedAggregate.sql UserDefinedAggregates
 
-:exit
+popd
