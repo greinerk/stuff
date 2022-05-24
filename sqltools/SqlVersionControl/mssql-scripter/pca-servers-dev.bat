b0e43419d03e3758d4e0ac4b3f0fb4a767bf230f
greinerk <greinerk@gmail.com>
2022-05-24 15:26:54 -0400

    call mssql-scripter once per server and object-type add rename script in powershell to remove object name from the name

--- sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev.bat
+++ sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev.bat
@@ -1,7 +1,7 @@
 
 pushd %~dp0
 
-call pca-sql-schema.bat awsql2019dev USAGG_DEV c:\pca\sql_schemas\mssql-scripter\
-call pca-sql-schema.bat awsql2019dev USALCO_FRQ_DEV c:\pca\sql_schemas\mssql-scripter\
+call pca-sql-schema-by-type.bat awsql2019dev USAGG_DEV c:\pca\sql_schemas\mssql-scripter\
+call pca-sql-schema-by-type.bat awsql2019dev USALCO_FRQ_DEV c:\pca\sql_schemas\mssql-scripter\
 
 popd
---
+++ sqltools/SqlVersionControl/mssql-scripter/pca-sql-schema-by-type.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem Parameters
+rem %1 server name
+rem %2 db name
+rem %3 SQL scripts root path
+
+del /Q /S %3\%1\%2\*
+
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\Tables                --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types Table
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\StoredProcedures      --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types StoredProcedure
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\Views                 --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types View
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\UserDefinedAggregates --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types UserDefinedAggregate
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\UserDefinedDataTypes  --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types UserDefinedDataType
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\UserDefinedFunctions  --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types UserDefinedFunction
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\UserDefinedTableTypes --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types UserDefinedTableTypes 
+call mssql-scripter -S %1 -d %2 -f %3\%1\%2\UserDefinedTypes      --continue-on-error --file-per-object --exclude-use-database --display-progress --exclude-headers --exclude-schemas SQL# --include-types UserDefinedType
