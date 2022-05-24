05ed35c25fecdbefee02f8fdfbf01621f425af0f
greinerk <greinerk@gmail.com>
2022-05-24 15:30:20 -0400

    simplify rename powershell script

--- sqltools/SqlVersionControl/mssql-scripter/fix-mssql-scripter-filenames.ps1
+++ sqltools/SqlVersionControl/mssql-scripter/fix-mssql-scripter-filenames.ps1
@@ -1,20 +1,8 @@
-
-# Start Script
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-function main()
-{
-	Get-ChildItem -Path "." -Filter "*.StoredProcedure.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.StoredProcedure.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.Table.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.Table.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.UserDefinedDataType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedDataType.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.UserDefinedAggregate.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedAggregate.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.UserDefinedFunction.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedFunction.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.UserDefinedTableType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedTableType.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.UserDefinedType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedType.sql','.sql' }
-	Get-ChildItem -Path "." -Filter "*.View.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.View.sql','.sql' }
-}
-
-#=============
-# Execute
-#=============
-main
+Get-ChildItem -Path "." -Filter "*.StoredProcedure.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.StoredProcedure.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.Table.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.Table.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.UserDefinedDataType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedDataType.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.UserDefinedAggregate.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedAggregate.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.UserDefinedFunction.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedFunction.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.UserDefinedTableType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedTableType.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.UserDefinedType.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.UserDefinedType.sql','.sql' }
+Get-ChildItem -Path "." -Filter "*.View.sql" -Recurse | Rename-Item -NewName {$_.name -replace '.View.sql','.sql' }
