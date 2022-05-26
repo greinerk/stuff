2e577475a413f62e9eacbdf6ce69eeef9ac95326
greinerk <greinerk@gmail.com>
2022-05-26 16:12:52 -0400

    Add all DEV databases on this instance show complete runtime at the end of the script

--- sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev-2.bat
+++ sqltools/SqlVersionControl/mssql-scripter/pca-servers-dev-2.bat
@@ -1,7 +1,34 @@
 
 @echo %date% %time%
 
+set curr_date=%date%
+set curr_time=%time%
+
 pushd %~dp0
+
+call pca-sql-schema.bat awsql2019dev ABET_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev AllianceAutoERP_Dev c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev GlobalReserves_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev JuniorAchievement_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev OGTrustVisionSystem_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev Printech_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev Sephora_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
+
+call pca-sql-schema.bat awsql2019dev TateBywater_DEV c:\pca\sql_schemas\mssql-scripter-2\
+call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
 
 call pca-sql-schema.bat awsql2019dev USAGG_DEV c:\pca\sql_schemas\mssql-scripter-2\
 call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
@@ -11,4 +38,5 @@ call git-update.bat c:\pca\sql_schemas\mssql-scripter-2\awsql2019dev
 
 popd
 
-@echo %date% %time%
+@echo Started  %curr_date% %curr_time%
+@echo Finished %date% %time%
